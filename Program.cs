3bb0ed36c79f62f80afe2591364bd113b41ae27d
mennatarekk566 <mennatarek273994@gmail.com>
2026-08-29 21:52:16 +0300

    2nd_answer

--- Program.cs
+++ Program.cs
@@ -3,16 +3,23 @@
     internal class Program
     {
         static void Main(string[] args)
-        {   
+        {
             //1st_anwer
-        //    #region
-        //    book b = new book();
-        //    b.tittle = "cukur";
-        //    b.page_number = 310;
+            //    #region
+            //book b = new book();
+            //    b.tittle = "cukur";
+            //    b.page_number = 310;
 
-        //    object aras = b;
-        //    Console.WriteLine(aras);
-        //    #endregion
-        
+            //    object aras = b;
+            //    Console.WriteLine(aras);
+            //    #endregion
+            #region
+            //2nd_answer
+            //Console.WriteLine(b.ToString());
+            //Console.WriteLine(b.GetHashCode());
+            //Console.WriteLine(b.Equals(b));
+            //Console.WriteLine(b.GetType());
+            #endregion
+        }
     }
 }
